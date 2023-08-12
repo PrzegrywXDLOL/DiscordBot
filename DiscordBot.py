9fc2b212e1298848f6e1d02bcd82b1d689d5e0ee
PrzegrywXDLOL <142092638+PrzegrywXDLOL@users.noreply.github.com>
2023-08-12 14:39:18 +0200

    Initial commit

--- DiscordBot.py
+++ DiscordBot.py
@@ -10,15 +10,12 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    elif message.content.find("!JD?") != -1:
-        await message.channel.send("```Jebać Disa Kurwe Zwisa Orka Skyrwysyna Syna Diabła```")
-
     elif message.content == "!ilechlopa":
         await message.channel.send(f"""```Tyle chłopa jest na serwie: 
 {id.member_count}```""")
 
     elif message.content.find("!60") != -1:
-        await message.channel.send("Mateusz To 60, Mateusz :right_fist: :left_fist: policja.")
+        await message.channel.send(f"""{message.author.mention} To 60, {message.author.mention} :right_fist: :left_fist: policja.""")
 
     elif message.content.find("!es") != -1:
         await message.channel.send(f"""{message.author.mention} es :sunglasses: :call_me:""")
@@ -30,11 +27,9 @@ async def on_message(message):
     elif message.content.find("!help") != -1:
         await message.channel.send("""```
 Komendy tego bota to:
-!JD? - wyzywa disa orka
 !ilechlopa - pokazuje ile osób jest na serwerze
-!60 - wzywa Mateusza D. od 60 i konfidentów
+!60 - wzywa od 60
 !es - pisze es
 !sieema - działa tylko w kanale sieema, pisze sieema```""")
-
 
 client.run("Nzg4ODI0NjIzMjIzMjc1NTUy.X9pILQ.2NFYTRvLgmxXVS4uQ14-v5k-a4c")
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # DiscordBot
- Python DiscordBot with predefined commands
+ Python DiscordBot with predefined commands.
 
-This bot was designed for polish server so commands and comments are in polish
+This bot was designed for polish server so commands and comments are in polish.
